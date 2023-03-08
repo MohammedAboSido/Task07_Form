bbbf1666ac5634fbd77941c9308f6adf90427e9e
Mohammed A. Abo Sido <abosido2@gmail.com>
2023-03-08 14:34:44 +0200

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "../pages/HomePage/HomePage";
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <HomePage/>
+      </div>
+    );
+  }
+}
+export default Home;
